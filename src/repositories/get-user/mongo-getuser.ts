0062d559bfc336fba73079d6a725a092b6394f19
Lukas22D <luk.look.s.rocha@gmail.com>
2023-10-25 14:46:22 -0300

    feat: ensure GetUsersMongoRepository get users

--- src/repositories/get-user/mongo-getuser.ts
+++ src/repositories/get-user/mongo-getuser.ts
@@ -1,15 +1,17 @@
 import { IGetUsersRepository } from "../../Controllers/get-user/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        fistName: "any_first_name",
-        lastName: "any_last_name",
-        email: "any_email",
-        password: "any_password",
-      },
-    ];
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
